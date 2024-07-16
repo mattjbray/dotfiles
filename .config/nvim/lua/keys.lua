1fa807cc7acbf2ad4245012001588a318dc62881
Matt Bray <mattjbray@gmail.com>
2024-07-16 21:38:19 +0100

    refactor(nvim): wincmds

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -12,29 +12,36 @@ M.setup = function()
 
   vim.keymap.set('n', '<leader>sc', ':nohlsearch<CR>', { desc = 'Clear search highlight' })
 
-  vim.keymap.set('n', '<leader>ws', ':wincmd s<CR>', { desc = 'Split window horizontally' })
-  vim.keymap.set('n', '<leader>wv', ':wincmd v<CR>', { desc = 'Split window vertically' })
-  vim.keymap.set('n', '<leader>wn', ':wincmd n<CR>', { desc = 'New window' })
-  vim.keymap.set('n', '<leader>wc', ':wincmd c<CR>', { desc = 'Close window' })
-  vim.keymap.set('n', '<leader>wo', ':wincmd o<CR>', { desc = 'Close other windows' })
+  local wincmds = {
+    { key = 's', desc = 'Split window horizontally' },
+    { key = 'v', desc = 'Split window vertically' },
+    { key = 'n', desc = 'New window' },
+    { key = 'c', as = 'd', desc = 'Close window' },
+    { key = 'o', desc = 'Close other windows' },
 
-  vim.keymap.set('n', '<leader>wh', ':wincmd h<CR>', { desc = 'Move cursor to left window' })
-  vim.keymap.set('n', '<leader>wj', ':wincmd j<CR>', { desc = 'Move cursor to window below' })
-  vim.keymap.set('n', '<leader>wk', ':wincmd k<CR>', { desc = 'Move cursor to window above' })
-  vim.keymap.set('n', '<leader>wl', ':wincmd l<CR>', { desc = 'Move cursor to right window' })
-  vim.keymap.set('n', '<leader>wp', ':wincmd p<CR>', { desc = 'Move cursor to previous window' })
+    { key = 'h', desc = 'Move cursor to left window' },
+    { key = 'j', desc = 'Move cursor to window below' },
+    { key = 'k', desc = 'Move cursor to window above' },
+    { key = 'l', desc = 'Move cursor to right window' },
+    { key = 'p', desc = 'Move cursor to previous window' },
 
-  vim.keymap.set('n', '<leader>wr', ':wincmd r<CR>', { desc = 'Rotate windows downwards/rightwards' })
-  vim.keymap.set('n', '<leader>wR', ':wincmd R<CR>', { desc = 'Rotate windows upwards/leftwards' })
-  vim.keymap.set('n', '<leader>wx', ':wincmd x<CR>', { desc = 'Swap window with next' })
+    { key = 'r', desc = 'Rotate windows downwards/rightwards' },
+    { key = 'R', desc = 'Rotate windows upwards/leftwards' },
+    { key = 'x', desc = 'Swap window with next' },
 
-  vim.keymap.set('n', '<leader>wT', ':wincmd T<CR>', { desc = 'Break window to a new tab' })
-  vim.keymap.set('n', '<leader>wH', ':wincmd H<CR>', { desc = 'Move window to far left' })
-  vim.keymap.set('n', '<leader>wL', ':wincmd L<CR>', { desc = 'Move window to far right' })
-  vim.keymap.set('n', '<leader>wJ', ':wincmd J<CR>', { desc = 'Move window to top' })
-  vim.keymap.set('n', '<leader>wK', ':wincmd K<CR>', { desc = 'Move window to bottom' })
+    { key = 'T', desc = 'Break window to a new tab' },
+    { key = 'H', desc = 'Move window to far left' },
+    { key = 'L', desc = 'Move window to far right' },
+    { key = 'J', desc = 'Move window to top' },
+    { key = 'K', desc = 'Move window to bottom' },
 
-  vim.keymap.set('n', '<leader>w=', ':wincmd =<CR>', { desc = 'Make all windows equally high and wide' })
+    { key = '=', desc = 'Make all windows equally high and wide' },
+  }
+
+  for _, entry in pairs(wincmds) do
+    local as = entry.as or entry.key
+    vim.keymap.set('n', '<leader>w' .. as, ':wincmd ' .. entry.key .. '<CR>', { desc = entry.desc })
+  end
 
   vim.keymap.set('n', '<leader>pp', ':Telescope projects<CR>', { desc = 'Projects (telescope)' })
 end
