af41d64960c4fc2b05f2d3e6d5848703ed3e2fe4
Matt Bray <mattjbray@gmail.com>
2024-07-20 18:10:57 +0100

    refactor(nvim): keys

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -1,39 +1,6 @@
 local M = {}
 
 M.setup = function()
-  -- require('which-key').add {
-  --   { '<leader>a', group = 'Apps' },
-  --   { '<leader>c', group = '[C]ode' },
-  --   { '<leader>d', group = '[D]ocument' },
-  --   { '<leader>e', group = 'Errors (diagnostics)' },
-  --   { '<leader>f', group = 'Files' },
-  --   { '<leader>g', group = 'Git' },
-  --   { '<leader>h', group = 'Help' },
-  --   { '<leader>m', group = 'Filetype' },
-  --   { '<leader>p', group = 'Projects' },
-  --   { '<leader>r', group = '[R]ename' },
-  --   { '<leader>s', group = '[S]earch' },
-  --   { '<leader>t', group = '[T]oggle' },
-  --   { '<leader>w', group = '[W]orkspace' },
-  --   { '<leader>x', group = 'Touble' },
-  -- }
-
-  local map_keys
-  map_keys = function(keys, path)
-    for key, data in pairs(keys) do
-      local this_path = (path or '') .. key
-      if data.group then
-        require('which-key').add { { this_path, group = data.group } }
-      end
-      if data.keys then
-        map_keys(data.keys, this_path)
-      end
-      if not data.group and not data.keys then
-        vim.keymap.set('n', this_path, data[1], { desc = data[2] })
-      end
-    end
-  end
-
   local keys = {
     ['<leader>'] = {
       keys = {
@@ -81,152 +48,188 @@ M.setup = function()
                 },
               },
             },
+            f = {
+              '<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>',
+              'File browser in buffer dir (telescope)',
+            },
+            r = {
+              require('telescope.builtin').oldfiles,
+              'Recent files (telescope)',
+            },
+          },
+        },
+        g = {
+          group = 'Git',
+        },
+        h = {
+          group = 'Help',
+          keys = {
+            h = {
+              require('telescope.builtin').help_tags,
+              'Browse help (telescope)',
+            },
+            k = {
+              require('telescope.builtin').keymaps,
+              'Browse key bindings (telescope)',
+            },
+          },
+        },
+        p = {
+          group = 'Projects',
+          keys = {
+            f = {
+              function()
+                require('telescope.builtin').find_files { hidden = true }
+              end,
+              'Find files in cwd (telescope)',
+            },
+            p = { '<cmd>Telescope projects<cr>', 'Projects (telescope)' },
+          },
+        },
+        w = {
+          group = 'Window',
+          keys = {
+
+            s = { '<cmd>wincmd s<cr>', 'Split window horizontally' },
+            v = { '<cmd>wincmd v<cr>', 'Split window vertically' },
+            n = { '<cmd>wincmd n<cr>', 'New window' },
+            c = { '<cmd>wincmd q<cr>', 'Close window' },
+            o = { '<cmd>wincmd o<cr>', 'Close other windows' },
+
+            h = { '<cmd>wincmd h<cr>', 'Move cursor to left window' },
+            j = { '<cmd>wincmd j<cr>', 'Move cursor to window below' },
+            k = { '<cmd>wincmd k<cr>', 'Move cursor to window above' },
+            l = { '<cmd>wincmd l<cr>', 'Move cursor to right window' },
+            p = { '<cmd>wincmd p<cr>', 'Move cursor to previous window' },
+
+            r = { '<cmd>wincmd r<cr>', 'Rotate windows downwards/rightwards' },
+            R = { '<cmd>wincmd R<cr>', 'Rotate windows upwards/leftwards' },
+            x = { '<cmd>wincmd x<cr>', 'Swap window with next' },
+
+            T = { '<cmd>wincmd T<cr>', 'Break window to a new tab' },
+
+            H = { '<cmd>wincmd H<cr>', 'Move window to far left' },
+            L = { '<cmd>wincmd L<cr>', 'Move window to far right' },
+            J = { '<cmd>wincmd J<cr>', 'Move window to top' },
+            K = { '<cmd>wincmd K<cr>', 'Move window to bottom' },
+
+            ['='] = {
+              '<cmd>wincmd =<cr>',
+              'Make all windows equally high and wide',
+            },
+          },
+        },
+        s = {
+          group = 'Search',
+          keys = {
+            c = { '<cmd>nohlsearch<cr>', 'Clear search highlight' },
           },
         },
       },
     },
   }
 
+  local lsp_keys = {
+    ['<leader>'] = {
+      keys = {
+        m = {
+          group = 'Lang / Filetype',
+          keys = {
+            g = {
+              group = 'Go to',
+              keys = {
+                g = {
+                  require('telescope.builtin').lsp_definitions,
+                  'definition (telescope)',
+                },
+                r = {
+                  require('telescope.builtin').lsp_references,
+                  'references (telescope)',
+                },
+                t = {
+                  require('telescope.builtin').lsp_type_definitions,
+                  'type definition (telescope)',
+                },
+              },
+            },
+            h = {
+              group = 'Help',
+              keys = {
+                h = { vim.lsp.buf.hover, 'Hover Documentation' },
+              },
+            },
+            l = {
+              group = 'Code lens',
+              keys = {
+                r = {
+                  function()
+                    vim.lsp.codelens.refresh()
+                  end,
+                  'refresh',
+                },
+                c = {
+                  function()
+                    vim.lsp.codelens.clear()
+                  end,
+                  'clear',
+                },
+                x = {
+                  function()
+                    vim.lsp.codelens.execute()
+                  end,
+                  'execute',
+                },
+              },
+            },
+            r = {
+              group = 'Refactor',
+              keys = {
+                n = { vim.lsp.buf.rename, 'rename' },
+              },
+            },
+            s = {
+              group = 'Search',
+              keys = {
+                d = {
+                  require('telescope.builtin').lsp_document_symbols,
+                  'document symbols (telescope)',
+                },
+                w = {
+                  require('telescope.builtin').lsp_dynamic_workspace_symbols,
+                  'workspace symbols (telescope)',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  }
+
+  local map_keys
+  map_keys = function(keys_, path)
+    for key, data in pairs(keys_) do
+      local this_path = (path or '') .. key
+      if data.group then
+        require('which-key').add { { this_path, group = data.group } }
+      end
+      if data.keys then
+        map_keys(data.keys, this_path)
+      end
+      if not data.group and not data.keys then
+        vim.keymap.set('n', this_path, data[1], { desc = data[2] })
+      end
+    end
+  end
+
   map_keys(keys)
-
-  vim.keymap.set('n', '<leader>pf', function()
-    require('telescope.builtin').find_files { hidden = true }
-  end, { desc = 'Search Files in cwd (telescope)' })
-  vim.keymap.set(
-    'n',
-    '<leader>ff',
-    '<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>',
-    { desc = 'File browser in buffer dir (telescope)' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>fr',
-    require('telescope.builtin').oldfiles,
-    { desc = 'Recent files (telescope)' }
-  )
-
-  vim.keymap.set(
-    'n',
-    '<leader>hh',
-    require('telescope.builtin').help_tags,
-    { desc = '[S]earch [H]elp' }
-  )
-  vim.keymap.set(
-    'n',
-    '<leader>hk',
-    require('telescope.builtin').keymaps,
-    { desc = '[S]earch [K]eymaps' }
-  )
 
   vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('keys-lsp-attach', { clear = true }),
-    callback = function(event)
-      local map = function(keys, func, desc)
-        vim.keymap.set(
-          'n',
-          keys,
-          func,
-          { buffer = event.buf, desc = 'LSP: ' .. desc }
-        )
-      end
-
-      require('which-key').add {
-        { '<leader>mg', group = 'Go to' },
-        { '<leader>mh', group = 'Help' },
-        { '<leader>ml', group = 'Code lens' },
-        { '<leader>mr', group = 'Refactor' },
-        { '<leader>ms', group = 'Search' },
-      }
-
-      map(
-        '<leader>mgg',
-        require('telescope.builtin').lsp_definitions,
-        'definitions (telescope)'
-      )
-      map(
-        '<leader>mgr',
-        require('telescope.builtin').lsp_references,
-        'references (telescope)'
-      )
-      map(
-        '<leader>mgt',
-        require('telescope.builtin').lsp_type_definitions,
-        'type definition (telescope)'
-      )
-      map('<leader>mhh', vim.lsp.buf.hover, 'Hover Documentation')
-      map('<leader>mrn', vim.lsp.buf.rename, 'rename')
-      map(
-        '<leader>msd',
-        require('telescope.builtin').lsp_document_symbols,
-        'document symbols (telescope)'
-      )
-      map(
-        '<leader>msw',
-        require('telescope.builtin').lsp_dynamic_workspace_symbols,
-        'workspace symbols (telescope)'
-      )
-
-      map('<leader>mlr', function()
-        vim.lsp.codelens.refresh()
-      end, 'refresh')
-      map('<leader>mlc', function()
-        vim.lsp.codelens.clear()
-      end, 'clear')
-      map('<leader>mlx', function()
-        vim.lsp.codelens.run()
-      end, 'execute')
+    callback = function()
+      map_keys(lsp_keys)
     end,
   })
-
-  vim.keymap.set(
-    'n',
-    '<leader>sc',
-    '<cmd>nohlsearch<CR>',
-    { desc = 'Clear search highlight' }
-  )
-
-  local wincmds = {
-    { key = 's', desc = 'Split window horizontally' },
-    { key = 'v', desc = 'Split window vertically' },
-    { key = 'n', desc = 'New window' },
-    { key = 'c', as = 'd', desc = 'Close window' },
-    { key = 'o', desc = 'Close other windows' },
-
-    { key = 'h', desc = 'Move cursor to left window' },
-    { key = 'j', desc = 'Move cursor to window below' },
-    { key = 'k', desc = 'Move cursor to window above' },
-    { key = 'l', desc = 'Move cursor to right window' },
-    { key = 'p', desc = 'Move cursor to previous window' },
-
-    { key = 'r', desc = 'Rotate windows downwards/rightwards' },
-    { key = 'R', desc = 'Rotate windows upwards/leftwards' },
-    { key = 'x', desc = 'Swap window with next' },
-
-    { key = 'T', desc = 'Break window to a new tab' },
-    { key = 'H', desc = 'Move window to far left' },
-    { key = 'L', desc = 'Move window to far right' },
-    { key = 'J', desc = 'Move window to top' },
-    { key = 'K', desc = 'Move window to bottom' },
-
-    { key = '=', desc = 'Make all windows equally high and wide' },
-  }
-
-  for _, entry in pairs(wincmds) do
-    local as = entry.as or entry.key
-    vim.keymap.set(
-      'n',
-      '<leader>w' .. as,
-      ':wincmd ' .. entry.key .. '<CR>',
-      { desc = entry.desc }
-    )
-  end
-
-  vim.keymap.set(
-    'n',
-    '<leader>pp',
-    ':Telescope projects<CR>',
-    { desc = 'Projects (telescope)' }
-  )
 end
 
 return M
