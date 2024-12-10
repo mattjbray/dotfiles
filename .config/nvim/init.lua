d45297ce94b72246c8f9e7f7faeb35b5a94509fa
Matt Bray <mattjbray@gmail.com>
2024-12-10 11:32:17 +0000

    feat(nvim/telescope): ivy theme for all pickers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -345,16 +345,8 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        pickers = {
-          find_files = {
-            theme = 'ivy',
-          },
-        },
+        defaults = require('telescope.themes').get_ivy {},
+        -- pickers = { },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
