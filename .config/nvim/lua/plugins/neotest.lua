5ca39cb918196019e217be0606dec26df50e17a0
Matt Bray <mattjbray@gmail.com>
2025-01-15 10:10:36 +0000

    feat(nvim): neotest

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,9 @@
+return {
+  'nvim-neotest/neotest',
+  requires = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
