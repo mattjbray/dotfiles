aadcc720763f46a7edfe12dd647500b41426ba77
Matt Bray <mattjbray@gmail.com>
2025-03-05 10:57:26 +0000

    feat(vim/conform): don't autoformat in diff mode

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -21,6 +21,12 @@ return {
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
+
+      -- Check if the buffer is in diff mode
+      if vim.wo.diff then
+        return nil
+      end
+
       return {
         timeout_ms = 500,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
